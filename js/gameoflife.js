e7a17e6c898c9e066f74cb845b0ef1b585e873c0
Marc Servat <marc.servat@sage.com>
2022-02-13 12:59:53 +0100

    feat: implement logic for get the neighbors of one cell

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -49,7 +49,12 @@ const printCells = (state) => {
   return accumulator;
 };
 
-const getNeighborsOf = ([x, y]) => {};
+const getNeighborsOf = ([x, y]) => {
+  return [
+    [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+    [x - 1, y], [x + 1, y],
+    [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]];
+};
 
 const getLivingNeighbors = (cell, state) => {};
 
